06ef6290406daa7018342386f04316ec99bb879a
PatrickLumenus <patrickaluy@gmail.com>
2021-09-14 08:13:39 -0400

    Defined DTO

---
+++ packages/dto/src/data/data.ts
@@ -0,0 +1,19 @@
+import { Serializable } from "@swindle/core";
+import { DataInterface } from "./data.interface";
+
+/**
+ * Data
+ * 
+ * Data is the base class for a Data Transfer Object.
+ */
+
+export abstract class Data implements DataInterface, Serializable {
+
+    constructor() {}
+
+    public abstract serialize(): string;
+
+    public toString(): string {
+        return this.serialize();
+    }
+}
